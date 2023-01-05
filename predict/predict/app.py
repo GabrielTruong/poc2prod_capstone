352583fa538309855887ced6752a6dbbc1954cc5
Gabriel TRUONG <lap.gabrieltruong@gmail.com>
2023-01-05 15:25:31 +0100

    add get for curl

--- predict/predict/app.py
+++ predict/predict/app.py
@@ -11,11 +11,20 @@ def home():
  
 @app.route("/", methods=['POST'])
 def get_prediction():
-    model = TextPredictionModel.from_artefacts("train/data/artefacts/train/2023-01-03-22-59-05")
+    model = TextPredictionModel.from_artefacts("train/data/artefacts/train/2023-01-04-17-25-24")
     text = request.form['text']
-    predictions = model.predict([text],top_k=3)
+    predictions = model.predict([text],top_k=1)
     
     return str(predictions)
+
+@app.route("/predict",methods=["GET"])
+def request_prediction():
+    model = TextPredictionModel.from_artefacts("train/data/artefacts/train/2023-01-04-17-25-24")
+    text = request.args.get('text')
+    predictions = model.predict([text],top_k=1)
+
+    return str(predictions)
+
 
 if __name__ == '__main__':
     app.run()
